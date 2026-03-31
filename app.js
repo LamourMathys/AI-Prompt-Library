ea03a60d5c729acfe08baaac78a81a3d8f457f1c
lamourmathys <mathys.lamoour@gmail.com>
2026-03-31 22:54:29 +0200

    feat:script went into app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const ulElement = document.querySelector("#prompt-list");
+
+    fetch("/prompt.json")
+      .then((response) => response.json())
+      .then((prompts) => {
+        for (const prompt of prompts) {
+           const liElement = document.createElement("li");
+           liElement.className = "display-card";
+
+           const t = document.createElement("div");
+           t.innerText = prompt.title;
+           liElement.appendChild(t);
+
+           const c = document.createElement("div");
+           c.innerText = prompt.category;
+           liElement.appendChild(c);
+
+           const p = document.createElement("div");
+           p.innerText = prompt.prompt;
+           liElement.appendChild(p);
+
+           ulElement.appendChild(liElement);
+        }
+      });
